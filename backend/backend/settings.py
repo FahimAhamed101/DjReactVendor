ad2545f9483e869cec197b68f71a7b577176549e
fahim <fahimahamedweb@gmail.com>
2025-05-05 00:19:01 +0600

    cors fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,7 +75,11 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React development server
+    "http://127.0.0.1:3000",
+    "https://djangoreactmultivendor.vercel.app",
+]
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
