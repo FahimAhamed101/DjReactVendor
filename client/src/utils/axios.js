7e37837c01490ac060b6a89a5818dbbf3adecf3e
fahim <fahimahamedweb@gmail.com>
2025-05-05 00:22:22 +0600

    vercel added

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const apiInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/v1/',
+    baseURL: 'https://dj-react-vendor.vercel.app/api/v1/',
     timeout: 20000,
 
     headers: {
